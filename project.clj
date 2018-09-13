4178f1a225780b0c02156cb40464e9587288efb3
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-13 18:06:41 +0200

    Formatting code.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject org.vladimir/js-lib "0.1.0"
-  :description    "JavaScript library"
-  :url            "http://gitlab:1610/VladimirMarkovic86/js-lib"
-  :license        {:name "Eclipse Public License"
-                   :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies   [[org.clojure/clojurescript "1.10.339"]
-                   [org.vladimir/ajax-lib "0.1.0"]
-                   [org.vladimir/htmlcss-lib "0.1.0"]
-                   ]
+  :description "JavaScript library"
+  :url "http://gitlab:1610/VladimirMarkovic86/js-lib"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojurescript "1.10.339"]
+                 [org.vladimir/ajax-lib "0.1.0"]
+                 [org.vladimir/htmlcss-lib "0.1.0"]
+                 ]
   :source-paths ["src/cljs"])
 
