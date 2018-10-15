96808d09f3be21f64c9a6ceee3fc8e8535ecb4cc
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:45:50 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
-(defproject org.vladimir/js-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.0"
   :description "JavaScript library"
-  :url "http://gitlab:1610/VladimirMarkovic86/js-lib"
+  :url "http://github.com/VladimirMarkovic86/js-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.vladimir/ajax-lib "0.1.0"]
-                 [org.vladimir/htmlcss-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.0"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/cljs"])
 
