ec0e299b1c06dc02e0aca18a9dc7128460b8c81c
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-15 11:03:47 +0100

    Task 54: Fixed po and ocr

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.4"
+(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.5"
   :description "JavaScript library"
   :url "http://github.com/VladimirMarkovic86/js-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.4"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.2"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.5"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.3"]
                  ]
 
   :min-lein-version "2.0.0"
