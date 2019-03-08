6cd901ef2158b87032f6a6973231466463d9cbaa
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-08 17:31:17 +0100

    Task 62: Refactored common-server, separated configuration for server and client side.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.5"
+(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.6"
   :description "JavaScript library"
   :url "http://github.com/VladimirMarkovic86/js-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.5"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.6"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.3"]
                  ]
 
