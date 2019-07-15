9fe86d093c4f8f042711b4070f7105f8a5a769a9
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:02:23 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,20 +1,18 @@
-(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.18"
+(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.19"
   :description "JavaScript library"
   :url "http://github.com/VladimirMarkovic86/js-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.8"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.9"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/cljs"]
   
-  :jar-exclusions [#"^public/"
-                   #"README.md$"
-                   #"LICENSE$"]
+  :jar-exclusions [#"^public/"]
 
   :plugins [[lein-cljsbuild  "1.1.7"]
             [lein-doo "0.1.11"]
