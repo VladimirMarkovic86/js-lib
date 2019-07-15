2daa3e871034a387f79d59faff6516452f7be69d
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 12:50:13 +0200

    Task 88: Removed public folder from tested clojurescript projects packed jar file.

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
-(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.16"
+(defproject org.clojars.vladimirmarkovic86/js-lib "0.1.17"
   :description "JavaScript library"
   :url "http://github.com/VladimirMarkovic86/js-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.11"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.7"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/cljs"]
+  
+  :jar-exclusions [#"^public/"]
 
   :plugins [[lein-cljsbuild  "1.1.7"]
             [lein-doo "0.1.11"]
